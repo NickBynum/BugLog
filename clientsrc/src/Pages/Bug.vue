72dc45b78bdf9f6f63e69210593dec9c87a9c8ad
Nick Bynum <exander@live.com>
2020-04-27 00:15:44 -0600

    status displays correct and color coded

--- clientsrc/src/Pages/Bug.vue
+++ clientsrc/src/Pages/Bug.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="Bug">
-        <tr >
+        <tr class="Bug">
           <th scope="row">{{bugData.title}}</th>
-          <td>{{bugData.description}}</td>
-          <td>{{bugData.status}}</td>
+          <td>{{bugData.creatorEmail}}</td>
+          <td v-if="bugData.status == true" class="text-danger">
+            Closed</td>
+            <td v-else class="text-success">Open</td>
           <td>{{bugData.updatedAt}}</td>
         </tr>
-  </div>
 </template>
 
 
